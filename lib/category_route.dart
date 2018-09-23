557cf56e774a9d52593c67ae652d74a89e934892
Mauricio Chirino <mauricio@3codegeeks.com>
2018-09-23 18:23:05 -0300

    Turn category_router into Stateful widget

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -16,12 +16,13 @@ final _backgroundColor = Colors.green[100];
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
 // TODO: Make CategoryRoute a StatefulWidget
-class CategoryRoute extends StatelessWidget {
-  const CategoryRoute();
+class CategoryRoute extends StatefulWidget {
 
   // TODO: Create State object for the CategoryRoute
+  @override
+  createState() => _CategoryRouteState();
 
-  static const _categoryNames = <String>[
+  final _categoryNames = <String>[
     'Length',
     'Area',
     'Volume',
@@ -32,7 +33,7 @@ class CategoryRoute extends StatelessWidget {
     'Currency',
   ];
 
-  static const _baseColors = <Color>[
+  final _baseColors = <Color>[
     Colors.teal,
     Colors.orange,
     Colors.pinkAccent,
@@ -54,7 +55,7 @@ class CategoryRoute extends StatelessWidget {
   }
 
   /// Returns a list of mock [Unit]s.
-  List<Unit> _retrieveUnitList(String categoryName) {
+  List<Unit> retrieveUnitList(String categoryName) {
     return List.generate(10, (int i) {
       i += 1;
       return Unit(
@@ -64,6 +65,26 @@ class CategoryRoute extends StatelessWidget {
     });
   }
 
+}
+
+class _CategoryRouteState extends State<CategoryRoute> {
+
+  List categories = <Category>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (var i = 0; i < widget._categoryNames.length; i++) {
+      categories.add(Category(
+        name: widget._categoryNames[i],
+        color: widget._baseColors[i],
+        iconLocation: Icons.cake,
+        units: widget.retrieveUnitList(widget._categoryNames[i]),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: Instead of re-creating a list of Categories in every build(),
@@ -71,21 +92,11 @@ class CategoryRoute extends StatelessWidget {
     // the list at initialization (in initState()).
     // This way, you also don't have to pass in the list of categories to
     // _buildCategoryWidgets()
-    final categories = <Category>[];
-
-    for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
 
     final listView = Container(
       color: _backgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: _buildCategoryWidgets(categories),
+      child: widget._buildCategoryWidgets(categories),
     );
 
     final appBar = AppBar(
