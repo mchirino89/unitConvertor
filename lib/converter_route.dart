de7c119c0b2c60a27cecac0ce529512cda07b0e2
Mauricio Chirino <mauricio@3codegeeks.com>
2018-09-23 17:01:18 -0300

    Turn convert_router into Stateful widget

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -13,7 +13,7 @@ import 'package:udacity_app/unit.dart';
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
 // TODO: Make ConverterRoute a StatefulWidget
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   /// This [Category]'s name.
   final String name;
 
@@ -33,15 +33,20 @@ class ConverterRoute extends StatelessWidget {
         assert(units != null);
 
   // TODO: Create State object for the ConverterRoute
+  @override
+  createState() => _ConverterRouteState();
 
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     // TODO: Once the build() function is inside the State object,
     // you'll have to reference this using `widget.units`
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
